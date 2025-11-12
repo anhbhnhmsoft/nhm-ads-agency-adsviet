cfec48e525dcf816abf7b086b189c43f09fcc84b
anhbhnhmsoft <anhbhnhmsoft@gmail.com>
2025-11-12 21:27:48 +0700

    AnhBH - intit service package

---
+++ app/Common/Constants/ServicePackage/ServicePackageFeature.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Common\Constants\ServicePackage;
+
+enum FeatureMeta:string
+{
+    const NEW_BM = "new_bm";
+    const FANPAGE_ATTACHED = "fanpage_attached";
+    const GUARANTEE = "guarantee";
+    const SUPPORT_247 = "support_247";
+
+}
---
+++ app/Http/Resources/ServicePackageResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ServicePackageResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'platform' => $this->platform,
+            'features' => $this->features, // Assuming features are already cast to array/json in model
+            'open_fee' => $this->open_fee,
+            'range_min_top_up' => $this->range_min_top_up,
+            'top_up_fee' => $this->top_up_fee,
+            'set_up_time' => $this->set_up_time,
+            'disabled' => $this->disabled,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
